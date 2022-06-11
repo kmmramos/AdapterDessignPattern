88a93f2390bc01b742cd10e4de19a9548fae52ce
Kirstin Megga Ramos <kirstinmegga.ramos@ph.softvision.global>
2022-06-11 16:18:20 +0800

    initial commit

---
+++ AdapterDesignPattern/Adapter/EmployeeAdapter.cs
@@ -0,0 +1,34 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace AdapterDesignPattern.Adapter
+//{
+//    internal class EmployeeAdapter
+//    {
+//    }
+//}
+
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace AdapterDesignPattern.Adapter
+{
+    public class EmployeeAdapter : EmployeeManager, IEmployee
+    {
+        public override string GetAllEmployees()
+        {
+            string returnXml = base.GetAllEmployees();
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(returnXml);
+            return JsonConvert.SerializeObject(doc, Newtonsoft.Json.Formatting.Indented);
+        }
+    }
+}
---
+++ AdapterDesignPattern/Target/IEmployee.cs
@@ -0,0 +1,26 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace AdapterDesignPattern.Target
+//{
+//    internal interface IEmployee
+//    {
+//    }
+//}
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterDesignPattern
+{
+    public interface IEmployee
+    {
+        string GetAllEmployees();
+    }
+}
